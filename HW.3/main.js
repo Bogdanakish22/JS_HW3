8732e4fce6d758701e6b5e2e97e696f07ea8d2ff
Kish Bogdana <kishbogdana22@gmail.com>
2025-02-11 18:18:17 +0100

    add

---
+++ HW.3/main.js
@@ -0,0 +1,22 @@
+const result = 5+5+"5";
+console.log(result);
+console.log(typeof result);
+
+const email = "kishbogdana22@gmail.com"
+const look = email.includes("@")
+console.log(look);
+console.log(email.length);
+
+let illname = "My";
+let forname = "name ";
+let ndwas = "is ";
+let fullName = illname + " " + forname + " " + ndwas;
+fullName += " Viktor";
+console.log(fullName);
+
+const userName = "Віктор"
+const pyment = 300
+console.log(alert(`Дякую, ${userName}! До сплати ${pyment} гривень`));
+
+
+ 
